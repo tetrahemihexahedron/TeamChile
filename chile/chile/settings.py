cfe94d64544151134b03fe1f4a28cf72b738e661
Sunny Fawcett <95454232+tetrahemihexahedron@users.noreply.github.com>
2026-04-18 19:40:24 -0600

    Add DB_NAME env variable

--- chile/chile/settings.py
+++ chile/chile/settings.py
@@ -78,7 +78,7 @@ WSGI_APPLICATION = "chile.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": "postgres",
+        "NAME": os.getenv("DB_NAME"),
         "USER": os.getenv("DB_USER"),
         "PASSWORD": os.getenv("DB_PASSWORD"),
         "HOST": os.getenv("DB_HOST"),
